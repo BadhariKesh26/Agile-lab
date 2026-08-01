80015c25e4e2317dae985e08738e49e946579134
Badhari Kesh <badharikesh246@gmail.com>
2026-08-01 08:46:02 +0530

    avg function

--- mark.py
+++ mark.py
@@ -7,9 +7,13 @@ def calculate_grade(marks):
         return "C"
     else:
         return "Fail"
+def average(marks_list):
+    return sum(marks_list) / len(marks_list)
 if __name__ == "__main__":
-    marks = 82
+    marks = [80, 85, 81]
+    avg = average(marks)
     print("Marks:", marks)
-    print("Grade:", calculate_grade(marks))
+    print("Average:", avg)
+    print("Grade:", calculate_grade(avg))
 
     
